b226aaf2ee270465c99f523d9a67637d3a582810
jiornojiovanni <gianni00palmieri@gmail.com>
2020-10-28 18:24:01 +0100

    4 spaces indentation instead of 2

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Cream API DB!');
+app.get("/", (req, res) => {
+    res.send("Cream API DB!");
 });
 
 app.listen(port, () => {
-  return console.log(`Server is listening on http://127.0.0.1:${port}`);
+    return console.log(`Server is listening on http://127.0.0.1:${port}`);
 });
