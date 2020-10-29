b4d5a27ff8099aa02531529954a2dce6668bcf1c
jiornojiovanni <gianni00palmieri@gmail.com>
2020-10-29 18:38:14 +0100

    added pug and fixed typo

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
-import steamcmd from 'steamcmd';
+import steamcmd from "steamcmd";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
     if(req.query.id != null) {
