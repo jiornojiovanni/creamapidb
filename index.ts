af0a48384dd13f6ede7ccb7e9d24fb6b10880b95
Giovanni Palmieri <diobrando@pop-os.localdomain>
2020-10-30 16:37:25 +0100

    various adjustment

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.urlencoded({ extended: true }));
@@ -16,21 +16,23 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    if(req.body.id != null) {
+    if (req.body.id != "") {
         steamcmd.getAppInfo(req.body.id)
-            .then( (result) => {
-                if(JSON.stringify(result) == "{}") {
-                    res.status(404).send("Non ho trovato niente! <br> <a href='/'>Ritenta</a>");                   
+            .then((result: any) => {
+                if (JSON.stringify(result) == "{}") {
+                    res.status(404).send("Non ho trovato niente! <br> <a href='/'>Ritenta</a>");
                 } else {
                     res.json(result);
                 }
             });
+    } else {
+        res.status(404).send("Non ho trovato niente! <br> <a href='/'>Ritenta</a>");
     }
 });
 
 app.listen(port, () => {
     steamcmd.download()
-        .then( () => {
+        .then(() => {
             steamcmd.touch();
         });
     console.log(`Server is listening on http://127.0.0.1:${port}`);
