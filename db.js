9297862800f7012a837c599a8c9e17b8dcaf70b2
Giovanni Palmieri <gianni00palmieri@gmail.com>
2020-11-03 20:39:42 +0100

    formatting

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
 
-exports.idExist = async function(id) {
+exports.idExist = async function (id) {
     const client = new Client();
     await client.connect();
     const res = await client.query(`select exists(select 1 from gamedata where id=${id}) as value`);
@@ -8,23 +8,23 @@ exports.idExist = async function(id) {
     return res.rows[0].value;
 };
 
-exports.cacheId = async function(id, name, path) {
+exports.cacheId = async function (id, name, path) {
     const client = new Client();
     await client.connect();
     try {
         await client.query(`insert into gamedata(id, name, path) values ('${id}', '${name}', '${path}')`);
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
 }
 
-exports.getData = async function(id) {
+exports.getData = async function (id) {
     const client = new Client();
     await client.connect();
     const res = await client.query(`select id, name, path from gamedata where id=${id}`);
     return {
-        id: res.rows[0].id, 
-        name: res.rows[0].name, 
+        id: res.rows[0].id,
+        name: res.rows[0].name,
         path: res.rows[0].path
     };
 }
