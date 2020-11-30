9a00e8fb6a612514f37f2efc53c617a642721e78
jiornojiovanni <gianni00palmieri@gmai.com>
2020-11-30 11:06:15 +0100

    changed steam search logic from regex to JSDOM

--- steamsearch.js
+++ steamsearch.js
@@ -1,6 +1,6 @@
 const https = require('https');
-const NameRegex = /div class="match_name">([^<>]*)<\/div>/g;
-const IDregex = /https:\/\/store.steampowered.com\/app\/(\d*)\//g;
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 exports.getSteamSearch = (text) => {
     return new Promise((resolve, reject) => {
@@ -15,9 +15,17 @@ exports.getSteamSearch = (text) => {
                 reject(new Error("Search Error"));
             }
             res.on('data', (data) => {
-                const ids = data2array(data, IDregex);
-                const names = data2array(data, NameRegex);
-                resolve({ id: ids, name: names });
+                let resultArray = [];
+                const dom = new JSDOM(data.toString());
+                dom.window.document.querySelectorAll('a').forEach((elm) => {
+                    resultArray.push( 
+                    {
+                        name: elm.querySelector('.match_name').textContent, 
+                        id: elm.getAttribute('data-ds-appid'), 
+                        img: elm.querySelector('img').getAttribute('src')
+                    });
+                });
+                resolve(resultArray);
             });
         });
         req.on('error', (err) => {
@@ -26,12 +34,4 @@ exports.getSteamSearch = (text) => {
         });
         req.end();
     });
-}
-
-function data2array(data, regex) {
-    let array = [];
-    while ((result = regex.exec(data)) != null) {
-        array.push(result[1]);
-    }
-    return array;
 }
