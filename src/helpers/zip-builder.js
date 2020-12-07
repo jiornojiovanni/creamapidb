7a9a44b2af437f47599a876a4a94d9234e936d9d
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-12-07 01:08:59 +0100

    Improved zip building.

--- src/helpers/zip-builder.js
+++ src/helpers/zip-builder.js
@@ -1,41 +1,22 @@
 import { createWriteStream } from 'fs';
 import archiver from 'archiver';
 import { file as _file } from 'tmp';
-import { promises as fsPromises } from 'fs';
 
-const buildZip = ({ id, gamepath }) => {
+const buildZip = ({ id, gamePath }) => {
     return new Promise((resolve, reject) => {
-        _file((err, path, fd) => {
+        _file((err, path) => {
             if (err) reject(err);
-
             const file = createWriteStream(path);
             const archive = archiver('zip');
 
-            archive.on('warning', function (err) {
-                if (err.code === 'ENOENT') {
-                    console.log(err);
-                } else {
-                    throw err;
-                }
-            });
-
-            archive.on('error', function (err) {
-                throw err;
-            });
+            file.on('close', function () { resolve(path); });
+            archive.on('warning', (err) => { reject(err); });
+            archive.on('error', (err) => { reject(err); });
 
             archive.pipe(file);
-
-            archive.append(getCreamINI(id), { name: gamepath + 'cream_api.ini' });
-            archive.file('bin/steam_api.dll', { name: gamepath + 'steam_api.dll' });
-            archive.file('bin/steam_api64.dll', { name: gamepath + 'steam_api64.dll' });
-            archive.file('bin/steam_api_o.dll', { name: gamepath + 'steam_api_o.dll' });
-            archive.file('bin/steam_api64_o.dll', { name: gamepath + 'steam_api64_o.dll' });
-            archive.finalize()
-                .then(() => {
-                    //We check if file exists before returning it as Archiver lib is a bit bugged.
-                    return fsPromises.stat(path);
-                })
-                .then(() => { resolve(path); });
+            archive.append(getCreamINI(id), { name: gamePath + 'cream_api.ini' });
+            archive.directory('bin/', gamePath);
+            archive.finalize();
         });
     });
 }
