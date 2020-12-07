07d5b6495584231aac9d06fbf3261a4f11627dca
Giovanni Palmieri <gianni00palmieri@gmail.com>
2020-12-07 10:47:50 +0100

    added more tests.

--- testing/test.js
+++ testing/test.js
@@ -7,7 +7,13 @@ describe("Testing Steam Search...", () => {
     it("Is it correctly returning only the maximum amount of elements from Steam-CMD?", async () => {
         const res = await getSteamSearch("counter strike");
         expect(res.length).to.equal(STEAM.MAX_RESULTS);
-    })
+    });
+
+    it("Is it returning an empty result when searching gibberish?", async () => {
+        const res = await getSteamSearch(".");
+        expect(res).to.be.empty;
+    });
+
 })
 
 describe("Testing Steam-CMD...", () => {
@@ -19,5 +25,15 @@ describe("Testing Steam-CMD...", () => {
         }
         const res = await searchSteamCMD(TEST2.id);
         expect(res).to.deep.equal(TEST2);
+    });
+
+    it("Is it throwing an error when searching for a game without the appropriate information?", async () => {
+        try {
+            //CSGO does not have correct information, so when called should always fail.
+            await searchSteamCMD(730);
+            throw new Error("Did not correctly throw inside the function.");
+        } catch (e) {
+            return expect(e.message).to.equal('Missing config in game infos.');
+        }
     })
 })
