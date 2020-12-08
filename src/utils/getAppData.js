e9dc410540c44c01189322b40b72b60c64590dcb
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-12-09 00:18:04 +0100

    Changed to async await for a better readability,

--- src/utils/getAppData.js
+++ src/utils/getAppData.js
@@ -2,24 +2,16 @@ import { getGameInfo, cacheGameInfo } from '../helpers/db';
 import searchSteamCMD from '../helpers/steam-cmd';
 
 const getAppData = (id) => {
-    return new Promise((resolve, reject) => {
-        getGameInfo(id)
-            .then((result) => {
-                if (result) {
-                    return resolve(result);
-                } else {
-                    return searchSteamCMD(id);
-                }
-            })
-            .then((result = null) => {
-                if (result) {
-                    resolve(result);
-                    return cacheGameInfo(result);
-                }
-            })
-            .catch((err) => {
-                reject(err);
-            });
+    return new Promise(async (resolve, reject) => {
+        try {
+            const infoFromDb = await getGameInfo(id);
+            if (infoFromDb) return resolve(infoFromDb);
+            const infoFromSteamCMD = await searchSteamCMD(id);
+            await cacheGameInfo(infoFromSteamCMD);
+            resolve(infoFromSteamCMD);
+        } catch(e) {
+            reject(e);
+        }
     });
 }
 
