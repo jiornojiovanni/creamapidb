7790785e666bc9af8cfd9f23eb165efcba1d4b85
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-12-08 12:02:05 +0100

    Added null value check.

--- src/routes/search.js
+++ src/routes/search.js
@@ -6,9 +6,7 @@ const router = Router();
 router.get('/search', (req, res) => {
     const term = req.query.term || '';
     steamSearch(term)
-        .then((data) => {
-            res.json(data);
-        })
+        .then((data) => { res.json(data || []); })
         .catch((err) => {
             res.json({ error: 'Internal server error'});
             console.log(err);
