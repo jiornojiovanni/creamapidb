88b174d153f544118ca92781250e9453a1a2807b
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-12-15 00:58:15 +0100

    Changed response parameters.

--- src/routes/ready.js
+++ src/routes/ready.js
@@ -8,7 +8,7 @@ router.post('/ready', (req, res) => {
     if (appid === null) return res.status(400).json({ code: 400, message: 'Bad request.' });
     getGameInfo(appid)
         .then((doc) => {
-            return res.status(200).json({ appid, ready: doc ? true : false });
+            return res.status(200).json({ appid, readyToDownload: doc ? true : false });
         })
         .catch((err) => {
             return res.status(500).json({ code: 500, message: err.message });
