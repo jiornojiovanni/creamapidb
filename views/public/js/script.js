6ecd514c6625877f578314d6757cf7dacdcdaf9a
ArciducaZagaria <pierluigizagaria@gmail.com>
2020-12-15 00:58:49 +0100

    Added forntend functions.

--- views/public/js/script.js
+++ views/public/js/script.js
@@ -1,58 +1,115 @@
-(function () {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        if (this.status === 200) {
-            updateGameResults(JSON.parse(this.responseText));
-        } else console.log('Request failed.  Returned status of ' + this);
+(function () {   
+    let searchController = null;
+
+    const shakeCheckboxes = () => {
+        document.querySelector('.checkbox-list').classList.add('error-shake');
+        document.activeElement.blur();
     };
 
-    var filterInput = (e) => {
+    const showLoadingScreen = (bool) => {
+        console.log('Loading screen ' + bool);
+    };
+
+    const filterInput = (e) => {
         if (e.code === 'Enter' || e.target.innerText.length > 32) e.preventDefault();
     };
 
-    var searchInput = (e) => {
+    const searchInput = (e) => {
         queryOnKeyPress(e.target.innerText);
         showGameResults(e.target.innerText.length !== 0);
     };
 
-    var queryOnKeyPress = (text) => {
-        xhr.open('GET', `/search?term=${encodeURI(text)}`);
-        xhr.send();
+    const queryOnKeyPress = (text) => {
+        if (searchController) searchController.abort();
+        searchController = new AbortController();
+        fetch(`/search`, { 
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ term: text }),
+                signal: searchController.signal
+            })
+            .then((res) => res.json())
+            .then((json) => updateGameResults(json))
+            .catch((err) => { if (err.name !== 'AbortError') console.error(err) });
     };
 
-    var updateGameResults = (results = []) => {
+    const updateGameResults = (results = []) => {
         document.querySelector('#game-list > .not-found').style.display = (results.length == 0) ? 'flex' : 'none';
         document.querySelectorAll('#game-list > .result:not(.not-found)').forEach((e, i) => {
             e.style.display = results[i] ? 'flex' : 'none';
             if (!results[i]) return;
-            e.appid = results[i].id;
+            e.dataset.appid = results[i].id;
             e.querySelector('.game-name').innerText = results[i].name;
             e.querySelector('.game-img > img').src = results[i].img;
         });
     };
 
-    var showGameResults = (bool) => {
+    const showGameResults = (bool) => {
         let classes = document.querySelector('#game-list').classList;
         bool ? classes.add('visible') : classes.remove('visible');
     };
 
-    var onClickResult = (e) => {
-        var params = {};
-        if (document.querySelector('#wrapper').checked) params.wrapper = 1;
-        if (document.querySelector('#dlcs').checked) params.dlcs = 1;
-        if (Object.keys(params).length === 0) {
-            document.querySelector('.checkbox-list').classList.add('error-shake');
-            document.activeElement.blur();
-            return;
-        }
-        let appid = e.currentTarget.appid;
-        window.location.href = `/download/${appid}?${new URLSearchParams(params).toString()}`;
+    const onClickResult = (e) => {
+        const { appid, wrapper, dlcs } = getParams(e.currentTarget);
+        if (!(wrapper || dlcs)) return shakeCheckboxes();
+        getReady(appid)
+            .then(({ readyToDownload }) => {
+                if (readyToDownload) return;
+                showLoadingScreen(true)
+                return buildReq(appid);
+            })
+            .then(() => {
+                showLoadingScreen(false);
+                console.log("download!");
+            })
+            .catch((err) => console.error(err));
+    };
+
+    const getParams = (DOMResult) => {
+        return params = {
+            appid: DOMResult.dataset.appid,
+            wrapper: document.querySelector('#wrapper').checked,
+            dlcs: document.querySelector('#dlcs').checked,
+        };
+    };
+
+    const getReady = (appid) => {
+        return new Promise((resolve, reject) => {
+            fetch(`/ready`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ appid }),
+                })
+                .then((res) => res.json())
+                .then((json) => {
+                    console.log(json);
+                    resolve(json)
+                })
+                .catch((err) => reject(err));
+        }); 
+    };
+
+    const buildReq = (appid) => {
+        return new Promise((resolve, reject) => {
+            fetch(`/build`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ appid }),
+                })
+                .then((res) => res.json())
+                .then((json) => {
+                    console.log(json);
+                    resolve(json)
+                })
+                .catch((err) => reject(err));
+        });
     };
 
     document.querySelector('.checkbox-list').addEventListener('animationend', (e) => e.target.classList.remove('error-shake'));
     document.querySelector('#search > .field').addEventListener('keypress', filterInput);
     document.querySelector('#search > .field').addEventListener('input', searchInput);
     document.querySelector('#search > .field').addEventListener('focusin', (e) => { showGameResults(e.target.innerText.length !== 0); });
+    document.querySelector('#search > .field').addEventListener('focusout', (e) => { showGameResults(e.target.innerText.length > 1); });
     document.querySelectorAll('#game-list > .result:not(.not-found)').forEach((e) => {
         e.addEventListener('click', onClickResult);
         e.addEventListener('keypress', (e) => { if (e.code === 'Enter') onClickResult(e); });
